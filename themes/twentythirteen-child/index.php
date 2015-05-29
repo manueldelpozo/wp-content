e47a13eb545cebc0bea74f0462877ffcd221db0a
manueldelpozo <mndelpozo@gmail.com>
2015-05-29 14:24:31 +0200

    include image thumbnail

--- themes/twentythirteen-child/index.php
+++ themes/twentythirteen-child/index.php
@@ -1,28 +1,22 @@
-<?php
-get_header();
-?>
+<?php get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
         <?php
-        
             $args = array( 'post_type' => 'product', 'posts_per_page' => 10 );
             $loop = new WP_Query( $args );
-
             while ( $loop->have_posts() ) : $loop->the_post();
         ?>
                 <div class="products">
+                    <?php 
+                    if ( has_post_thumbnail() ) 
+                        the_post_thumbnail();
+                    ?>
                     <h3><?php the_title() ?></h3>
                     <div class="entry-content">
                         <?php the_content() ?>
                     </div>
                 </div>
-        <?php endwhile; 
-        
-       
-
-        ?>
+        <?php endwhile; ?>
     </main><!-- .site-main -->
 </div><!-- .content-area -->
-<?
-get_footer();
-?>
+<? get_footer(); ?>
