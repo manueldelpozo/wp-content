26c5b6cf8c9bd08d88f4fe5d4f916eeb5ba9a754
manueldelpozo <mndelpozo@gmail.com>
2015-05-29 12:36:06 +0200

    Try only one loop

--- themes/twentythirteen-child/index.php
+++ themes/twentythirteen-child/index.php
@@ -1,47 +1,21 @@
 <?php
-/**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- */
+get_header();
+echo "hola";
+?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+        <?php
+            $args = array( 'post_type' => 'product' );
 
-get_header(); ?>
+        ?>
+                <div class="products">
 
-	<div id="primary" class="content-area">
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <h3><?php the_title() ?></h3>
+                    <p><?php the_content() ?></p>
+                </div>
 
-		<main id="main" class="site-main" role="main">
-			<div class="page-title"><?php the_title() ?></div>
-
-			<?php
-			$args = array( 'post_type' => 'product' );
-			$second_loop = new WP_Query( $args ); 
-			// Start the loop.
-			if ( $second_loop -> have_posts() ) : the_post();
-				while ( $second_loop -> have_posts() ) : $second_loop -> the_posts();
-				?>
-				<div class="products">
-					<?php 
-					if ( has_post_thumbnail() ) 
-						the_post_thumbnail();
-					?>
-					<h3><?php the_title() ?></h3>
-					<p><?php the_content() ?></p>
-				</div>
-				<?php
-				endwhile;// End the second loop.
-			endif;
-			wp_reset_postdata();
-			?>
-
-		</main><!-- .site-main -->
-
-		<?php endif; endwhile; ?>
-	</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
-
-<?php get_footer(); ?>
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+<?
+get_footer();
+ ?>
