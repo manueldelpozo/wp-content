6ec091a03123fac2cd1a16924b81913a775001d2
manueldelpozo <mndelpozo@gmail.com>
2015-05-29 12:51:03 +0200

    Add action in the function.php

--- themes/twentythirteen-child/functions.php
+++ themes/twentythirteen-child/functions.php
@@ -5,4 +5,12 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
+// Show posts of 'post', 'page' and 'movie' post types on home page
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+function add_my_post_types_to_query( $query ) {
+  if ( is_home() && $query->is_main_query() )
+    $query->set( 'post_type', array( 'post', 'page', 'movie' ) );
+  return $query;
+}
+
 ?>
