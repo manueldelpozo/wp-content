9bdf9210f3f7790f51cd999dba1a8976d202f865
manueldelpozo <mndelpozo@gmail.com>
2015-06-11 11:05:44 +0200

    Order by newest manually

--- themes/twentythirteen-child/page-magazine.php
+++ themes/twentythirteen-child/page-magazine.php
@@ -8,7 +8,7 @@ get_header(); ?>
             <?php
             $categories = get_categories(); 
             $do_not_duplicate = array();
-            $publicationSeconds = array();
+            $publicationDates = array();
             $postList = array();
 
             foreach ( $categories as $category ) {
@@ -28,6 +28,8 @@ get_header(); ?>
                     while ( $loop->have_posts() ) { 
                         $loop->the_post();
                         $publicationDateUnix = get_the_time("U");
+                        //$publicationSeconds[] = get_the_time(i) * 60 + get_the_time(G) * 3600 + get_the_time(j) * 86400 + get_the_time(Y) * 31536000;
+                        $publicationDates[] = $publicationDateUnix;
                         $postList[ $publicationDateUnix ] = $post;
                     }
                 endif;
@@ -36,40 +38,44 @@ get_header(); ?>
                 // Obtain newest post by the biggest unix time number
                 //$newest = array_search( max( $publicationSeconds ), $publicationSeconds);
             }
-            // Order array by key
+            // Order array by key DESC
             krsort($postList);
-            var_dump($postList);
-
+            // Order dates DESC
+            rsort($publicationDates);
+            
             // Display array of Objects
-            //foreach( $postList => $postItem ) {
-                //echo $postItem['ID'];
-                //$do_not_duplicate[] = $postItem['ID'];
-                //$link = get_permalink( $postItem['ID'] );
-                //$format = get_post_format( $postItem['ID'] );
-                //$link_readmore = "../category/".get_the_category( $postItem['ID'] )[0]->name;
-                
-                //$publicationSeconds[] = get_the_time(i) * 60 + get_the_time(G) * 3600 + get_the_time(j) * 86400 + get_the_time(Y) * 31536000;
-                //$publicationSeconds[] = get_the_time("U");
-                /*    
+            foreach( $publicationDates as $dates ) {
+
+     
+
+                $myID = $postList[$dates]->ID;
+                $mytitle = $postList[$dates]->post_title; 
+                $myexcerpt = $postList[$dates]->post_content; 
+                $do_not_duplicate[] = $myID;
+                $link = get_permalink( $myID );
+                $format = get_post_format( $myID );
+                $category = get_the_category( $myID )[0]->name;
+                $link_readmore = "../category/".$category;
+                 
             ?>
                     <a href="<?php echo $link; ?>">
-                        <div id="post-<?php //the_ID(); ?>" <?php //post_class( 'category-listing' ); ?> style="float:left;display:inline;width:30%;height:400px;border-bottom:5px solid grey;background-color:white;padding:0;margin:1.666%;position:relative;">
-                            <a href="<?php //echo $link_readmore; ?>" style="position:absolute;background-color:white;color:grey;text-transform:uppercase;padding:3px 10px;margin-top:20px;"><?php //echo $category->name; ?></a>
+                        <div id="post-<?php echo $myID ?>" <?php post_class( 'category-listing' ); ?> style="float:left;display:inline;width:30%;height:400px;border-bottom:5px solid grey;background-color:white;padding:0;margin:1.666%;position:relative;">
+                            <a href="<?php echo $link_readmore; ?>" style="position:absolute;background-color:white;color:grey;text-transform:uppercase;padding:3px 10px;margin-top:20px;"><?php echo $category; ?></a>
                             <?php 
                             //if ( has_post_thumbnail() ) { 
-                                //$image_id = get_post_thumbnail_id(); 
-                                //$image_url = wp_get_attachment_image_src($image_id,'large'); 
-                                //$image_url = $image_url[0]; 
-                                //echo "<div class='image' style='background: url(".$image_url.") no-repeat center center; background-size:cover; width:100%; height:300px;'>";
+                                $image_id = get_post_thumbnail_id( $myID ); 
+                                $image_url = wp_get_attachment_image_src($image_id,'large'); 
+                                $image_url = $image_url[0]; 
+                                echo "<div class='image' style='background: url(".$image_url.") no-repeat center center; background-size:cover; width:100%; height:300px;'>";
                             //}
                             ?>
                             </div>
                             <div class="entry-content" style="height:70px;max-width:100%;background-color:white;padding:15px;overflow:hidden;position:absolute;bottom:10px;">  
-                                <h3 class="title" style="margin-top:0;"><center><?php echo $postItem['post_title'];//the_title() ?></center></h3>
-                                <div class="content"><?php $postItem['post_excerpt'];//the_excerpt(); ?></div>
+                                <h3 class="title" style="margin-top:0;"><center><?php echo $mytitle; ?></center></h3>
+                                <div class="content"><?php echo $myexcerpt; ?></div>
                             </div>
                             <div class="read-more" style="height:25px;background-color:white;position:absolute;left:0;bottom:0">      
-                                <a style="text-transform:uppercase;padding-left:10px;color:#CCC;font-size:0.9em;" href="<?php //echo $link_readmore ?>">Read More</a>
+                                <a style="text-transform:uppercase;padding-left:10px;color:#CCC;font-size:0.9em;" href="<?php echo $link_readmore ?>">Read More</a>
                             </div>
                         </div>
                     </a>
@@ -83,18 +89,13 @@ get_header(); ?>
                             }
                         });
                     </script>
-            <?php*/ //} ?>
+            <?php 
+            } 
+            ?>
                 </section>
           
             <script type="text/javascript">
-                jQuery(".category-listing").each( function( index ){ 
-                    if( index == <?php echo $newest; ?> ) {
-                        jQuery(this).css("width","63%");
-                        //var urlMed = jQuery(this).find(".image").css("background");
-                        //var urlLarge = urlMed.replace("medium", "large");
-                        //jQuery(this).find(".image").css("background",urlLarge);
-                    }
-                });
+                jQuery(".category-listing").first().css("width","63%");
             </script>
             </div>
             <div style="clear:both;"></div>
