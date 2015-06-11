2924374678445e533fe1bb53f36f3f7b087dcdd6
manueldelpozo <mndelpozo@gmail.com>
2015-06-11 12:07:02 +0200

    Bigger width by php

--- themes/twentythirteen-child/page-magazine.php
+++ themes/twentythirteen-child/page-magazine.php
@@ -25,7 +25,7 @@ get_header(); ?>
                     while ( $loop->have_posts() ) { 
                         $loop->the_post();
                         $publicationDateUnix = get_the_time("U");
-                        //$publicationSeconds[] = get_the_time(i) * 60 + get_the_time(G) * 3600 + get_the_time(j) * 86400 + get_the_time(Y) * 31536000;
+                        //$publicationSeconds = get_the_time(i) * 60 + get_the_time(G) * 3600 + get_the_time(j) * 86400 + get_the_time(Y) * 31536000;
                         $postList[ $publicationDateUnix ] = $post;
                     }
                 endif;
@@ -34,9 +34,11 @@ get_header(); ?>
             }
             // Order array by key DESC
             krsort($postList);
-            
+            // Initialize index
+            $index = 0;
             // Display array of Objects
             foreach( $postList as $dates => $postItem ) {
+                
                 $myID = $postList[$dates]->ID;
                 $mytitle = $postList[$dates]->post_title; 
                 $do_not_duplicate[] = $myID;
@@ -45,18 +47,19 @@ get_header(); ?>
                 $myexcerpt = wp_trim_words( $postList[$dates]->post_content, $num_words = 32, $more );; 
                 $format = get_post_format( $myID );
                 $category = get_the_category( $myID )[0]->name;
-                $link_readmore = "../category/".str_replace(' ', '', $category);    
+                $link_readmore = "../category/".str_replace(' ', '', $category); 
+                $width = 30;  
+                if ( $index == 0 )
+                    $width = 63; 
             ?>
                     <a href="<?php echo $link; ?>">
-                        <div id="post-<?php echo $myID ?>" <?php post_class( 'category-listing' ); ?> style="float:left;display:inline;width:30%;height:400px;border-bottom:5px solid grey;background-color:white;padding:0;margin:1.666%;position:relative;">
+                        <div id="post-<?php echo $myID ?>" <?php post_class( 'category-listing' ); ?> style="float:left;display:inline;width:<?php echo $width; ?>%;height:400px;border-bottom:5px solid grey;background-color:white;padding:0;margin:1.666%;position:relative;">
                             <a href="<?php echo $link_readmore; ?>" style="position:absolute;background-color:white;color:grey;text-transform:uppercase;padding:3px 10px;margin-top:20px;"><?php echo $category; ?></a>
                             <?php 
-                            //if ( has_post_thumbnail() ) { 
-                                $image_id = get_post_thumbnail_id( $myID ); 
-                                $image_url = wp_get_attachment_image_src($image_id,'large'); 
-                                $image_url = $image_url[0]; 
-                                echo "<div class='image' style='background: url(".$image_url.") no-repeat center center; background-size:cover; width:100%; height:300px;'>";
-                            //}
+                            $image_id = get_post_thumbnail_id( $myID ); 
+                            $image_url = wp_get_attachment_image_src($image_id,'large'); 
+                            $image_url = $image_url[0]; 
+                            echo "<div class='image' style='background: url(".$image_url.") no-repeat center center; background-size:cover; width:100%; height:300px;'>";
                             ?>
                             </div>
                             <div class="entry-content" style="height:70px;max-width:100%;background-color:white;padding:15px;overflow:hidden;position:absolute;bottom:10px;">  
@@ -79,13 +82,10 @@ get_header(); ?>
                         });
                     </script>
             <?php 
+                $index++;
             } 
             ?>
                 </section>
-          
-            <script type="text/javascript">
-                jQuery(".category-listing").first().css("width","63%");
-            </script>
             </div>
             <div style="clear:both;"></div>
 		</main><!-- .site-main -->
